592bd6abcadc2ca4d95c51405e8c6cba623dd23e
XavierPs90 <xavialonsops@gmail.com>
2021-04-14 21:17:20 -0600

    Cambio de base de datos

--- QuerySql/Funciones.sql
+++ QuerySql/Funciones.sql
@@ -39,6 +39,50 @@ select from f_modificar_carrera('derecho', 8)
 select * from t_carrera
 
 --++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-----------------------TABLA-MATRICULA------------------------
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+create or replace function f_insertar_matricula() returns void
+as 
+$$
+declare
+	codigo character varying := 'mat' + cast(i_fk_estudiante as character varying);
+	fecha date := current_date;
+begin 
+	insert into t_matricula (vc_codigo, vc_fecha) values (codigo, fecha);
+end;
+$$
+language sql
+--------------------------------------------------------------
+select f_insertar_matricula()
+select * from t_usuario
+--------------------------------------------------------------
+--------------------------------------------------------------
+
+create or replace function f_eliminar_usuario (_pk integer) returns void
+as
+$$
+	delete from t_usuario where i_pk_usuario = _pk
+$$
+language sql
+--------------------------------------------------------------
+select * from f_eliminar_usuario(3)
+select * from t_carrera
+--------------------------------------------------------------
+--------------------------------------------------------------
+
+create or replace function f_modificar_usuario_especifico
+(_pk integer, _campo character varying) returns void
+as 
+$$
+	update t_usuario set vc_ = _carrera where i_pk_carrera = _pk;
+$$
+language sql
+--------------------------------------------------------------
+select * from f_modificar_carrera('administracion', 6)
+select * from t_carrera
+
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ------------------------TABLA-USUARIO-------------------------
 --++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
